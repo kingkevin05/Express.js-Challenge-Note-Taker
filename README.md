f4420de098be22c0550c705f12f7d716562db8cc
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-04-18 22:08:34 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # Express.js-Challenge-Note-Taker
+
+## Description 
+  
+  This is Note Taker, an application that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+  
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  
+  ## Installation
+  
+  
+ Make sure to have [**Node.js**](https://nodejs.org/en/download/) downloaded.
+
+- [**Express**](https://expressjs.com/) is the most widely used Node.js server framework, this allows us to create our own server-side APIs.
+
+
+- [**Generate Unique ID**](https://www.npmjs.com/package/generate-unique-id) is used to generate a unique random ID string.
+
+Open a command prompt at the project's directory and run:
+
+```
+npm init -y  
+npm install express 
+npm install generate-unique-id
+```
+
+  
+  ## Usage
+
+  Click [**here**](https://thawing-lake-64142.herokuapp.com/) to view deployed application
+  
+
+  
+  *Instructions and examples for use:*
+  
+1. When a user opens the Note Taker, they are presented with a landing page containing a link to a notes page.
+2. When the user clicks on the link to the notes page, they're presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note's text in the right-hand column.
+3. When the user enters a new note title and the note's text, a save icon appears in the navigation at the top of the page.
+4. When the user clicks on the save icon, the new note they've entered is saved and appears in the left-hand column with the other existing notes.
+5. When the user clicks on an existing note in the list in the left-hand column, that note appears in the right-hand column.
+6. When the user clicks on the write icon in the navigation at the top of the page, they're presented with empty fields to enter a new note and the note's text in the right-hand column.
+
+![screenshot1](https://github.com/kingkevin05/Express.js-Challenge-Note-Taker/blob/main/images/Screenshot1.png)
+
+![screenshot2](https://github.com/kingkevin05/Express.js-Challenge-Note-Taker/blob/main/images/Screenshot2.png)
+
+
+  ## License
+  
+ ISC License
+  
+  ---
+  
+  ## Questions?
+  
+  ![Developer Profile Picture](https://avatars.githubusercontent.com/u/75460766?v=4) 
+  
+  If you have any question please feel free to reach out:
+ 
+  GitHub: [@kingkevin05](https://api.github.com/users/kingkevin05)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,27 @@
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
 const fs = require("fs");
 
 const generateUniqueId = require("generate-unique-id");
 
-const editNote = (updatedNotesArray) => {
-  fs.writeFile("./db/db.json", JSON.stringify(updatedNotesArray), (err) => {
+const editNote = updatedNotesArray => {
+  fs.writeFile("./db/db.json", JSON.stringify(updatedNotesArray), err => {
     if (err) throw err;
   });
 };
 
-// ROUTING
-module.exports = (app) => {
-  // GET REQUEST
-  // Setup the /api/notes GET route
+module.exports = app => {
   app.get("/api/notes", (req, res) => {
-    // Read the db.json file and return all saved notes as JSON.
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) throw err;
-      // Parse the JSON string into a JavaScript object
+
       res.json(JSON.parse(data));
     });
   });
 
-  // POST REQUEST
-  // Setup the /api/notes post route
   app.post("/api/notes", (req, res) => {
-    // Receives a new note, adds it to the db.json file, returns the new note to the client
     const newNote = req.body;
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) throw err;
-      // Parse the JSON string into a JavaScript object
+
       const notesArr = JSON.parse(data);
       newNote.id = generateUniqueId({ length: 10 });
       notesArr.push(newNote);
@@ -46,14 +37,12 @@ module.exports = (app) => {
     });
   });
 
-  // DELETE REQUEST
-  // Setup the /api/notes/:id delete route
   app.delete("/api/notes/:id", (req, res) => {
     const deleteId = req.params.id;
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) throw err;
       let notesArr = JSON.parse(data);
-      // removes the note with the given id property
+
       for (let i = 0; i < notesArr.length; i++) {
         if (notesArr[i].id === deleteId) {
           notesArr.splice(i, 1);
@@ -65,8 +54,6 @@ module.exports = (app) => {
     });
   });
 
-  // PUT REQUEST
-  // Setup the /api/notes/:id put route
   app.put("/api/notes/:id", (req, res) => {
     const editId = req.params.id;
 
@@ -75,19 +62,17 @@ module.exports = (app) => {
 
       let notesArr = JSON.parse(data);
 
-      let selectedNote = notesArr.find((note) => note.id === editId);
+      let selectedNote = notesArr.find(note => note.id === editId);
 
-      // check if found
       if (selectedNote) {
         let updatedNote = {
-          title: req.body.title, // set value of `title` get from req
-          text: req.body.text, // set value of `text` get from req
+          title: req.body.title,
+          text: req.body.text,
           id: selectedNote.id,
         };
-        //  find index at which the item is stored in the array
+
         let targetIndex = notesArr.indexOf(selectedNote);
 
-        //  replace object data with `updatedNote` object
         notesArr.splice(targetIndex, 1, updatedNote);
 
         res.sendStatus(204);
